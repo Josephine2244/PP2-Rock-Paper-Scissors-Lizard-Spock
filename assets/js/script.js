5f5bb863b29b74092b05b28e5bb623b362280e03
Josephine2244 <>
2023-09-18 15:13:17 +0000

    Add media queries and start adding to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const displayPlayerChoice = document.getElementById('player-choice');
+const displayHostChoice = document.getElementById('host-choice');
+const displayResult = document.getElementById('result');
