eeaf95b26aebe1e8673f70bc8b802351572d5d8a
Josephine2244 <>
2023-09-18 15:15:31 +0000

    structure js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
 const displayPlayerChoice = document.getElementById('player-choice');
 const displayHostChoice = document.getElementById('host-choice');
 const displayResult = document.getElementById('result');
+const playerOptions = document. getElementsByClassName('player');
+let playerChoice;
+let hostChoice;
+let result;
+
+function randomHostChoice() {
+
+}
+
+function whoWins() {
+
+}
+
+function playerScore() {
+
+}
+
+function hostScore() {
+
+}
+
+function resetScore() {
+    
+}
